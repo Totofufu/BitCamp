f4742023f362cba7b0b3515e07d720562c9ddbd0
Rohan Varma <Rohan@Rohans-MacBook-Pro-2.local>
2014-04-05 03:38:45 -0400

    fixed the css issues so it looks cool

--- helloflask/hello.py
+++ helloflask/hello.py
@@ -1,11 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def homePage():
-  return render_template('base.html')
+	return render_template('base.html')
+
+@app.route('/faq.html')
+def faqPage():
+	return render_template('faq.html')
+
+@app.route('/mostpopular.html')
+def popularPage():
+	return render_template('mostpopular.html')
+
+@app.route('/submit.html')
+def submitPage():
+	return render_template('submit.html')
 
 if __name__ == '__main__':
     app.run()
