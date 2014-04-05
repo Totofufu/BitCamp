636b80e532d30a551ed6c2a0f309338b0f13fe24
Rohan Varma <Rohan@Rohans-MacBook-Pro-2.local>
2014-04-05 04:22:13 -0400

    testing db

--- helloflask/hello.py
+++ helloflask/hello.py
@@ -1,10 +1,12 @@
-import os
-from flask import Flask, render_template
+import os, pymongo, postsDAO
+from flask import Flask, render_template, redirect
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def homePage():
+	posts_list = posts.get_posts()
 	return render_template('base.html')
 
 @app.route('/faq.html')
@@ -18,6 +20,19 @@ def popularPage():
 @app.route('/submit.html')
 def submitPage():
 	return render_template('submit.html')
+
+@app.route('/newpost', methods = ['POST'])
+def insert_newpost():
+	name = bottle.request.forms.get("name")
+	text = bottle.request.forms.get("text")
+	posts.insert_post(name, text)
+	app.redirect('/')
+
+connection_string = "mongodb://heroku:bd0ddad12dc3420f84c31f91cc65fc7d@oceanic.mongohq.com:10011/app23759697"
+
+connection = pymongo.MongoClient(connection_string)
+database = connection.Posts
+posts = decisionPostsDAO.decisionPostsDAO(database)
 
 if __name__ == '__main__':
     app.run()
---
+++ helloflask/postsDAO.py
@@ -0,0 +1,19 @@
+import string
+
+class DecisionPostsDAO(object):
+
+	def __init__(self, database):
+		self.db = database
+		self.posts = database.Posts
+
+	def get_posts(self):
+		postList = []
+		for post in self.posts.find():
+			print post
+			postList.append((post['name'], post['text']))
+		return postList
+
+	def insert_post(self, name, text):
+		newPost = {'name': name, "text": text}
+		self.Posts.insert(newPost)
+		
