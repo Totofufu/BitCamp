404133557ac27eddcc90a9ab0b7151ff58e1f267
Rohan Varma <Rohan@Rohans-MacBook-Pro-2.local>
2014-04-05 03:16:10 -0400

    added basic webpage design layout from eileen

--- helloflask/hello.py
+++ helloflask/hello.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-  return 'Hello World!'
+def homePage():
+  return render_template('base.html')
 
 if __name__ == '__main__':
     app.run()
